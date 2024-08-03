bd521abee3b0a3244ac9bcc8998a036a90523def
Omar Hernandez <40805237+omarin-ho@users.noreply.github.com>
2024-08-02 18:56:13 -0600

    Proyecto

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+def celsius_to_fahrenheit(celsius):
+    calculo = celsius * 9/5 + 32
+    return calculo
+
+def fahrenheit_to_celsius(fahrenheit):
+    calculo = (fahrenheit -32 )* 5/9
+    
+@app.route('/convert', methods = ['POST'])
+def convert_temp():
+    data = request.get_json()
+    input_temp=data.get('temperature')
+    
+    if data['type'] == 'celsius':
+        result = celsius_to_fahrenheit(input_temp)
+        output_unit = 'fahrenheit'
+    
+    if data['type'] == 'fahrenheit':
+        result = fahrenheit_to_celsius(input_temp)
+        output_unit = 'celsius'
+        
+    return jsonify({"temperature": result, "unit": output_unit})
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
